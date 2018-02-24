1eb2d14c6130e3485be1e902b99f2798942fccc9
Wendy Ann Li <wendyli@cs.uchicago.edu>
2018-02-24 11:01:24 -0600

    code

--- car.py
+++ car.py
@@ -9,17 +9,6 @@ def offset(undist, left_fit, right_fit):
 	"""
 	Calculate vehicle offset from lane center
 	"""
-	# Calculate vehicle center offset in pixels
-	bottom_y = undist.shape[0] - 1
-	bottom_x_left = left_fit[0]*(bottom_y**2) + left_fit[1]*bottom_y + left_fit[2]
-	bottom_x_right = right_fit[0]*(bottom_y**2) + right_fit[1]*bottom_y + right_fit[2]
-	vehicle_offset = undist.shape[1]/2 - (bottom_x_left + bottom_x_right)/2
-
-	# Convert pixel offset to meters
-	xm_per_pix = 3.7/700 # meters per pixel in x dimension
-	vehicle_offset *= xm_per_pix
-
-	return vehicle_offset
 
 
 
