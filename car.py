55ebe91f57380f5cf6cabfdef5363a0d7bf737a4
Wendy Ann Li <wendyli@cs.uchicago.edu>
2018-02-27 14:23:07 -0600

    car.py from lab

--- car.py
+++ car.py
@@ -4,6 +4,7 @@ from detector_wrapper import DetectorWrapper
 import numpy as np
 import cv2
 import math
+import time
 
 def offset(frame,mid_x):
     """
@@ -31,6 +32,7 @@ def endpoint_case1(height, left_fit, right_fit):
     '''
     both parabolas intersect height of frame
     '''
+    print left_fit[0], left_fit[1], left_fit[2]
     x1 = left_fit[0] * height**2 + left_fit[1] * height + left_fit[2]
     x2 = right_fit[0] * height**2 + right_fit[1] * height + right_fit[2]
     return x2 - x1
@@ -55,25 +57,47 @@ def main():
     try:
         while True:
             front_wheels.turn_straight()
+            time.sleep(1)
             success, ret = detector.detect()
+            if not success:
+                continue
             frame = ret[0]
             mid_x = ret[1]
             left_fit = ret[2]
             right_fit = ret[3]
             ploty = ret[4]
-            width,height = frame.shape
-            x2 = endpoint_case1(height/2,left_fit,right_fit)
-            xy_dist = dist(width/2,0,x2,height/2)
+        
+            width = frame.shape[0]
+            print "width", width
+            height = frame.shape[1]
+            print "height", height
+            x2 = endpoint_case1(height/4,left_fit,right_fit)
+            xy_dist = dist(width/2,0,x2,height/4)
+            
+            newx = abs(width/2 - mid_x)
+            deg = math.atan(height/newx) * 180 / math.pi 
+
+
             #adj = x2 - off
-            x_dist = x2 - mid_x
+            print "mid_x", mid_x
+            print "x2", x2
+            x_dist = x2 - width/2
             # Determine whether degree should be positive or negative
             if x_dist >= 0: 
                 left = 0
             else:
                 left = 1
-            degree = math.asin(x_dist/dy_dist)
-
+            print x_dist, xy_dist
+           # degree = math.asin(abs(x_dist)/xy_dist) * 180 / math.pi
+            degree =  deg
+            if left:
+                degree = -(80 - deg)
+            else:
+                degree = 80 - deg
             #driving
+            print "driving"
+            print "degree", degree
+            back_wheels.speed = 25
             front_wheels.turn_rel(degree)
             back_wheels.forward()
             time.sleep(1)
